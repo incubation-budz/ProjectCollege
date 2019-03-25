7b269048459a37f212ddda5c31a9a79d9f5742e1
anis1123 <hamalanis1@gmail.com>
2019-03-25 13:48:14 +0545

    changed welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-    <link rel="icon" href="{{asset('/img/apexidentity.png')}}" type="image/gif" sizes="16x16">
+    <link rel="icon" href="{{asset('/img/apexidentity.png')}}" type="image/gif" sizes="16x32">
 
 		<!--Font-->
 
